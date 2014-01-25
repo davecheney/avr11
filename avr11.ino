1a0b3132b5a76641ba1767ae6a178bfaacb6c283
Dave Cheney <dave@cheney.net>
2014-01-25 17:34:47 +1100

    Avoid calling setjmp on every loop()
    Instruction time reduced to 132usec/instr

--- avr11.ino
+++ avr11.ino
@@ -9,8 +9,8 @@
 
 
 int serialWrite(char c, FILE *f) {
-    Serial.write(c);
-    return 0;
+  Serial.write(c);
+  return 0;
 }
 
 pdp11::unibus unibus;
@@ -18,7 +18,7 @@ pdp11::unibus unibus;
 void setup(void)
 {
   // setup all the SPI pins, ensure all the devices are deselected
-    pinMode(4, OUTPUT); digitalWrite(4, HIGH);
+  pinMode(4, OUTPUT); digitalWrite(4, HIGH);
   pinMode(6, OUTPUT); digitalWrite(6, HIGH);
   pinMode(7, OUTPUT); digitalWrite(7, HIGH);
   pinMode(10, OUTPUT); digitalWrite(10, HIGH);
@@ -40,40 +40,43 @@ uint16_t instcounter;
 void loop() {
   uint16_t vec = setjmp(trapbuf);
   if (vec == 0) {
-            if ((itab[0].vec > 0) && (itab[0].pri >= ((PS>>5)&7))) {
-                handleinterrupt(itab[0].vec);
-                uint8_t i;
-                for (i = 0; i < ITABN-1; i++) {
-                        itab[i] = itab[i+1];
-                }
-                itab[ITABN-1].vec = 0;
-                itab[ITABN-1].pri = 0;
-        }
+    loop0();
+  }  else {
+    trapat(vec);
+  }
+}
 
+void loop0() {
+  while (true) {
+    if ((itab[0].vec > 0) && (itab[0].pri >= ((PS >> 5) & 7))) {
+      handleinterrupt(itab[0].vec);
+      uint8_t i;
+      for (i = 0; i < ITABN - 1; i++) {
+        itab[i] = itab[i + 1];
+      }
+      itab[ITABN - 1].vec = 0;
+      itab[ITABN - 1].pri = 0;
+    }
     cpustep();
     if (INSTR_TIMING) {
       if (++instcounter == 0) {
-          Serial.println(millis());
+        Serial.println(millis());
       }
     }
     clkcounter++;
-        if (clkcounter > 39999) {
-                clkcounter = 0;
-                LKS |= (1 << 7);
-                if (LKS&(1<<6)) {
-                        interrupt(INTCLOCK, 6);
-                }
-        }
-
-  } 
-  else {
-    trapat(vec);
+    if (clkcounter > 39999) {
+      clkcounter = 0;
+      LKS |= (1 << 7);
+      if (LKS & (1 << 6)) {
+        interrupt(INTCLOCK, 6);
+      }
+    }
+    unibus.cons.poll();
+    rkstep();
   }
-  unibus.cons.poll();
-  rkstep();
 }
 
 void panic() {
   printstate();
-  while(true) delay(1);
+  while (true) delay(1);
 }
